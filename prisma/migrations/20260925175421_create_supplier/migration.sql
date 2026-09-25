f7245275b33bfe92db8df5a1428f5b9aad39b4cd
Rodrigo <rodrigolorenzzonbasso@gmail.com>
2026-09-25 15:05:01 -0300

    add purchases do db

---
+++ prisma/migrations/20260925175421_create_supplier/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `Product` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateTable
+CREATE TABLE "Supplier" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Supplier_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Supplier_name_key" ON "Supplier"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
